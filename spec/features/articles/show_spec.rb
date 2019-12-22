ad44ded1f4bf2caee0c114b26c89c742d34a3996
Daniel <danmoran.pro@gmail.com>
2019-12-22 10:48:57 -0700

    add show page for articles

--- spec/features/articles/show_spec.rb
+++ spec/features/articles/show_spec.rb
@@ -1,10 +1,9 @@
-require 'rails-helper'
+require 'rails_helper'
 
 RSpec.describe "user sees one article" do
    describe "they link from the articles index" do
      it "displays information for one article" do
        article_1 = Article.create!(title: "Title 1", body: "New Body")
-       article_2 = Article.create!(title: "Title 2", body: "Body 2")
 
       visit articles_path
 
